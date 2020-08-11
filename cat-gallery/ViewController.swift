300bc411c4fd2a20c044c14e1ffcda649af32d84
Otávio Souza <otavioses>
2020-08-11 16:42:18 -0300

    Add new iOS project

---
+++ cat-gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cat-gallery
+//
+//  Created by Otávio Souza on 11/08/20.
+//  Copyright © 2020 otavioses. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ cat-galleryTests/cat_galleryTests.swift
@@ -0,0 +1,34 @@
+//
+//  cat_galleryTests.swift
+//  cat-galleryTests
+//
+//  Created by Otávio Souza on 11/08/20.
+//  Copyright © 2020 otavioses. All rights reserved.
+//
+
+import XCTest
+@testable import cat_gallery
+
+class cat_galleryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
